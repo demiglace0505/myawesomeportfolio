5994dd056ae028034965142029cd5143104b1098
demiglace0505 <demiglace0505@gmail.com>
2021-10-21 16:53:17 +0800

    removed gatsby-plugin-netlify, update gatsby to 2.32.13

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
     author: `@tomphill`,
   },
   plugins: [
-    `gatsby-plugin-netlify`,
+    // `gatsby-plugin-netlify`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
